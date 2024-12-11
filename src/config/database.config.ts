4c0862760e5a3091e9ed6752a10f58803e218e3b
Pavithran Rajkumar <rajkumar.pavithran@gene.com>
2024-12-12 02:50:36 +0530

    fixes

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -12,6 +12,12 @@ const sequelize = new Sequelize(process.env.DB_URL || '', {
     acquire: 30000,
     idle: 10000,
   },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 export const testConnection = async () => {
