0b6cb06c43d1703371b6654696c2e1e4c17860ed
Pavithran Rajkumar <rajkumar.pavithran@gene.com>
2024-12-12 02:47:46 +0530

    DB Config changes

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,19 +2,9 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
-console.log({
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  logging: process.env.NODE_ENV === 'development' ? console.log : false,
-});
-const sequelize = new Sequelize({
+
+const sequelize = new Sequelize(process.env.DB_URL || '', {
   dialect: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5432'),
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
   logging: process.env.NODE_ENV === 'development' ? console.log : false,
   pool: {
     max: 5,
